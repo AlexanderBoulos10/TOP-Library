600d287cf86b0bcd90dc32323fc1d9b60b555143
Alexander Boulos <alexander.boulos10@gmail.com>
2023-06-20 02:26:15 -0400

    Added form functionality, inputs displayed in list

--- app.js
+++ app.js
@@ -7,21 +7,51 @@ class Book {
 		this.numOfPages = numOfPages;
 		this.isRead = isRead;
 	}
-	// info() {
-	// 	if (this.isRead) {
-	// 		console.log(
-	// 			`${this.title} by ${this.author}, ${this.numOfPages}, done reading.`
-	// 		);
-	// 	} else {
-	// 		console.log(
-	// 			`${this.title} by ${this.author}, ${this.numOfPages} pages, not read yet.`
-	// 		);
-	// 	}
-	// }
 
 	addBookToLibrary() {
 		myLibrary.push(this);
 	}
+
+	isReadText() {
+		if (this.isRead) {
+			return "Read";
+		} else {
+			return "Not Read";
+		}
+	}
 }
 
 const book1 = new Book("Think and Grow Rich", "Napolean Hill", 290, false);
+
+const form = document.querySelector("form");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const numOfPages = document.querySelector("#pages");
+const checkIfRead = document.querySelector("#check");
+const bookList = document.querySelector(".allBooks");
+
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+	let book = new Book(
+		title.value,
+		author.value,
+		numOfPages.value,
+		checkIfRead.checked
+	);
+
+	book.addBookToLibrary();
+	displayBook(book);
+
+	title.value = "";
+	author.value = "";
+	numOfPages.value = "";
+	checkIfRead.checked = false;
+});
+
+const displayBook = (book) => {
+	let currentLi = document.createElement("li");
+	currentLi.textContent = `${book.title} by ${book.author}, ${
+		book.numOfPages
+	}, ${book.isReadText()}`;
+	bookList.append(currentLi);
+};
