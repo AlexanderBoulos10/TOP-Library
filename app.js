d6447b677b8ad18ecf187a266d3840288a943f3e
Alexander Boulos <alexander.boulos10@gmail.com>
2023-06-20 13:45:27 -0400

    Can now display and delete book info

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ const author = document.querySelector("#author");
 const numOfPages = document.querySelector("#pages");
 const checkIfRead = document.querySelector("#check");
 const bookList = document.querySelector(".allBooks");
+const addBook = document.querySelector(".addBook");
+const cancelForm = document.querySelector(".cancelButton");
 
 form.addEventListener("submit", function (e) {
 	e.preventDefault();
@@ -46,12 +48,68 @@ form.addEventListener("submit", function (e) {
 	author.value = "";
 	numOfPages.value = "";
 	checkIfRead.checked = false;
+	form.style.display = "none";
 });
 
 const displayBook = (book) => {
 	let currentLi = document.createElement("li");
-	currentLi.textContent = `${book.title} by ${book.author}, ${
-		book.numOfPages
-	}, ${book.isReadText()}`;
+
+	let content = document.createElement("p");
+	content.textContent = book.title;
+	currentLi.append(content);
+
+	content = document.createElement("p");
+	content.textContent = `By ${book.author}`;
+	currentLi.append(content);
+
+	content = document.createElement("p");
+	content.textContent = `${book.numOfPages} Pages`;
+	currentLi.append(content);
+
+	let buttonContainer = document.createElement("div");
+	buttonContainer.classList.add("listButtons");
+
+	content = document.createElement("p");
+	content.textContent = book.isReadText();
+
+	let readButton = document.createElement("button");
+	readButton.classList.add("readButton");
+	if (!book.isRead) {
+		readButton.classList.toggle("readButton");
+	}
+	readButton.textContent = "Read";
+	readButton.addEventListener("click", () => {
+		book.isRead = !book.isRead;
+		readButton.classList.toggle("readButton");
+		content.textContent = book.isReadText();
+	});
+	buttonContainer.append(readButton);
+
+	let deleteButton = document.createElement("button");
+	deleteButton.classList.add("deleteButton");
+	deleteButton.textContent = "Delete";
+	deleteButton.addEventListener("click", () => {
+		bookList.removeChild(currentLi);
+	});
+	buttonContainer.append(deleteButton);
+
+	currentLi.append(buttonContainer);
+	currentLi.append(content);
 	bookList.append(currentLi);
 };
+
+addBook.addEventListener("click", () => {
+	form.style.display = "inline-block";
+});
+
+cancelForm.addEventListener("click", () => {
+	title.value = "";
+	author.value = "";
+	numOfPages.value = "";
+	checkIfRead.checked = false;
+	form.style.display = "none";
+});
+
+const deleteItem = document.querySelector(".deleteButton");
+
+// deleteItem.addEventListener("click", () => {});
